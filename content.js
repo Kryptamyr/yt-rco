f644ccb2abfd517ac5eaf348a249490a90a27e88
Corbino <corbin.gossett2204@gmail.com>
2025-12-14 14:56:54 -0600

    Simplified initialization in content.js

--- content.js
+++ content.js
@@ -1,17 +1,13 @@
-chrome.storage.sync.get(['enabled'], (result) => {
-  if (result.enabled === false) return;
+let lastUrl = location.href;
+new MutationObserver(() => {
+  if (location.href === lastUrl) return;
+  lastUrl = location.href;
 
-  let lastUrl = location.href;
-  new MutationObserver(() => {
-    if (location.href === lastUrl) return;
-    lastUrl = location.href;
+  if (location.pathname !== "/watch") return;
+  if (location.search.includes("rco=1")) return;
 
-    if (location.pathname !== "/watch") return;
-    if (location.search.includes("rco=1")) return;
-
-    const url = new URL(location.href);
-    url.searchParams.set("rco", "1");
-    history.replaceState({}, "", url);
-    location.reload();
-  }).observe(document, { subtree: true, childList: true });
-});
+  const url = new URL(location.href);
+  url.searchParams.set("rco", "1");
+  history.replaceState({}, "", url);
+  location.reload();
+}).observe(document, { subtree: true, childList: true });
